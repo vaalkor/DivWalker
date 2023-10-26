7b1f58a0472274cfd012b18599a3494adfaf7375
vaalkor <groggydoggie@protonmail.com>
2023-10-26 23:08:46 +0100

    Added optimized walker files so it works on firefox (thanks valacar)

--- ConvertImages.ps1
+++ ConvertImages.ps1
@@ -1,5 +1,7 @@
-﻿foreach($x in Get-ChildItem "*.png")
-{
-    $data = [convert]::ToBase64String((get-content $x -encoding byte))
-    $data | Out-File -FilePath "x64_$($x.Name)".TrimEnd(".png")
-}
+﻿# Outputs a bunch of pngs as a javascript blob for inserting into the divwalker.js script.
+"let images = [`n"
+(Get-ChildItem "*.png" | %{
+    $data = [convert]::ToBase64String((get-content $_ -encoding byte))
+    "`"data:image/png;base64, $data`""
+}) -Join ",`n"
+"];"
